5cff80923e2b3de8cc7bab4b26a82ed990438965
Brad Landers <brad@bradlanders.com>
2012-08-24 15:26:32 -0400

    Remove bundler code and require correct dispatcher class

--- lib/benchtool.rb
+++ lib/benchtool.rb
@@ -1,5 +1,4 @@
 require 'deep-symbolizable/deep-symbolize'
-require 'bundler'
 require 'fileutils'
 require 'highline/import'
 require 'open4'
@@ -9,16 +8,10 @@ require 'rubygems'
 require 'yaml'
 
 require 'benchtool/app-config'
-require 'benchtool/benchmarker'
 require 'benchtool/ab-cmd-builder'
 require 'benchtool/curl-cmd-builder'
+require 'benchtool/dispatcher'
 require 'benchtool/extensions'
 require 'benchtool/helpers'
 require 'benchtool/version'
 require 'benchtool'
-
-require 'ap'
-
-Bundler.setup
-Bundler.require
-
